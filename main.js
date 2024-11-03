6747b2429ab83ed80ee157599b6af8d0d9ad5d4a
David Layne <davidlayne@usf.edu>
2024-11-03 18:20:48 -0500

    Dynamically display tickets on the page.

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 const ticketList = document.getElementById('ticketList');
 const errorNote = document.getElementById('error');
 
+
 // 2. Fetch Tickets using Async/Await and handle Errors
 // Async function to fetch customer and order data
-async function displayCustomerOrders(userId) {
+async function displayCustomerOrders(tickets) {
     try {
-        const customerResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`);
+        const customerResponse = await fetch(`https://jsonplaceholder.typicode.com/posts`);
         if (!customerResponse.ok) {
             throw new Error('Failed to fetch customer data');
         }
         const customer = await customerResponse.json();
         console.log('Customer:', customer);
 
-        const ordersResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`);
+        const ordersResponse = await fetch(`https://jsonplaceholder.typicode.com/posts`);
         if (!ordersResponse.ok) {
             throw new Error('Failed to fetch orders');
         }
         const orders = await ordersResponse.json();
         console.log('Orders:', orders);
 
-        orders[0].forEach(ticket => {
+        orders.forEach(ticket => {
             // 3. display tickets dynamically
-            const listItem = document.createElement('li');
-            listItem.textContent = `
-            Ticket Id: ${ticket.id}, Customer Name: ${ticket.userId}, Issue Description: ${ticket.title}, Details: ${ticket.body}
+            const listItem = document.createElement('div');
+            listItem.innerHTML = 
+            `
+            <ul><b>Ticket Id:</b> ${ticket.id}
+            <br><b>Name:</b> ${ticket.userId}
+            <br><b>Description:</b> ${ticket.title}
+            <br><b>Details:</b> ${ticket.body}
+            <hr width="50%">
+
+            </ul>
             `;
             ticketList.appendChild(listItem);
         });
@@ -36,4 +44,7 @@ async function displayCustomerOrders(userId) {
 }
 
 // Fetch and display tickets
-displayCustomerOrders(2);
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(tickets => {
+        displayCustomerOrders(tickets);
+    })
